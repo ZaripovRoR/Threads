42edf53bfb9d834dc0918d28c38e7271f4987365
ZaripovRoR <robertzaripov12345@gmail.com>
2022-07-15 18:52:15 +0300

    Threads1

---
+++ Threds/Threds/Methods.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Threds
+{
+    internal class Methods
+    {
+        public static void Print1()
+        {
+            int a=0;
+            for (int i = 0; i < 1000; i++)
+            {
+                 a++;
+                Console.WriteLine(a);
+                Thread.Sleep(100);
+            }
+        }
+        public static void Print2()
+        {
+            while (true)
+            {
+                Console.WriteLine("Xnjns nen pf,sk");
+                Thread.Sleep(1000);
+            }
+            
+        }
+        public static void Print3()
+        {
+            while (true)
+            {
+                Console.WriteLine("~_~");
+                Thread.Sleep(800);
+            }
+            
+        }
+        public static void Print4()
+        {
+            while (true)
+            {
+                Console.WriteLine("зачем ты тут?");
+                Thread.Sleep(10);
+            }
+            
+        }
+    }
+
+}
---
+++ Threds/Threds/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading;
+using Threds;
+var Thread1 = new Thread(Methods.Print1);
+var Thread2 = new Thread(Methods.Print2);
+var Thread3 = new Thread(Methods.Print3);
+var Thread4 = new Thread(Methods.Print4);
+Thread1.Start();
+Thread2.Start();
+Thread3.Start();
+Thread4.Start();
+int a;
+for (int i = 0; i < 1000; i++)
+{
+    a=i*2;
+    Console.WriteLine(a);
+    Thread.Sleep(500);
+}
